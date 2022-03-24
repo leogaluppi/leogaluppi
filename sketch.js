fda0533a27eee648d0805f03d509b38beac02dbc
leogaluppi <99920936+leogaluppi@users.noreply.github.com>
2022-03-24 18:23:23 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var ship1
+var ship2
+var ship3
+var ship4
+function preload(){
+ship3 = loadImage("ship-1.png")
+ship3 = loadImage("ship-2.png")
+ship3 = loadImage("ship-3.png")
+ship3 = loadImage("ship-4.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+
+ 
+}
