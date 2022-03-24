d601dbfc3de68fb1ecbb734552e6910db057ba78
leogaluppi <99920936+leogaluppi@users.noreply.github.com>
2022-03-24 19:05:08 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,66 @@
-var ship1
-var ship2
-var ship3
-var ship4
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+var chao
 function preload(){
-ship3 = loadImage("ship-1.png")
-ship3 = loadImage("ship-2.png")
-ship3 = loadImage("ship-3.png")
-ship3 = loadImage("ship-4.png")
+  //loadImage (carregarImagem) da pista)
+  //loadAnimation (carregarAnimação) de corrida para o menino
+loadImage ("Runner-1.png")
+loadImage ("Runner-3.png")
+loadImage ("Runner-2.png")
 }
 
 function setup(){
-  createCanvas(400,400);
   
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
+  
+ 
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  createCanvas(400,400);
+ //crie um sprite para a pista 
+//adicione uma imagem para a pista
+//Faça com que a pista seja um fundo que se move dando a ela velocity Y.
+path.scale=1.2;
+
+//crie um sprite de menino
+//adicione uma animação de corrida para ele
+boy.scale=0.08;
+  
+//crie um limite à esquerda
+leftBoundary=createSprite(0,0,100,800);
+//defina visibilidade como falsa para o limite à esquerda
+
+//crie um limite à direita
+rightBoundary=createSprite(410,0,100,800);
+//defina visibilidade como falsa para o limite à direita
 }
 
 function draw() {
-  background("blue");
-    drawSprites();
-
- 
+  background(0);
+  path.velocityY = 4;
+  
+  // mover o menino com o mouse usando mouseX
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  // colidir o menino com os limites invisíveis da esquerda e da direita
+  
+  //código para redefinir o fundo
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+  drawSprites();
 }
